76c0b9780fc82e964ee959c1eca1d0d3b9c6bdc2
f r e s h <fresh.thit@me.com>
2024-09-21 22:40:23 +0700

    update react-native-elements version & change babel preset

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,10 @@
-// module.exports = {
-//   presets: ['module:metro-react-native-babel-preset'],
-// };
-
-/* eslint-disable */
-
 const path = require('path');
 
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['module:metro-react-native-babel-preset'],
-    // presets: ['babel-preset-expo'],
+    // presets: ['module:metro-react-native-babel-preset'],
+    presets: ['babel-preset-expo'],
     plugins: [
       [
         'module-resolver', {
