71f82d9f10819d504f6f4b9532ec587cd61fcb8f
f r e s h <fresh.thit@me.com>
2025-05-31 04:17:32 +0700

    refactor: use <VKInfoCard/> for Streching Instruction

--- components/Streching/Instruction.tsx
+++ components/Streching/Instruction.tsx
@@ -4,20 +4,25 @@ import {
 } from 'react-native'
 
 import StyledText from 'react-native-styled-text'
+import { StrechingData } from '@app/utils/content/streching'
 import { SharedStyle } from '../styles'
 import { renderOrderedList } from '../utils'
 import i18n from 'i18n-js'
+import VKInfoCard from '../VKInfoCard'
 
 const Instruction: React.FC = () => {
-  const TITLE = i18n.t('streching_note_title')
-  const CONTENT: string[] = i18n.t('streching_note')
+  // const TITLE = StrechingData.Note;
+  // const CONTENT: string[] = i18n.t('streching_note')
+  // return (
+  //   <View style={SharedStyle.InfoCard}>
+  //     <StyledText style={SharedStyle.Title}>
+  //       { TITLE }
+  //     </StyledText>
+  //     { renderOrderedList(CONTENT) }
+  //   </View>
+  // )
   return (
-    <View style={SharedStyle.InfoCard}>
-      <StyledText style={SharedStyle.Title}>
-        { TITLE }
-      </StyledText>
-      { renderOrderedList(CONTENT) }
-    </View>
+    <VKInfoCard {...StrechingData.Notes} />
   )
 };
 
